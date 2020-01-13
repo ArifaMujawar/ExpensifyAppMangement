320ba87faac7ba2b6533b84c6850a6a893aeff85
ArifaMujawar <arifabegam.mujawar@gmail.com>
2020-01-13 09:29:43 +0200

    removed space

--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,8 @@ module.exports = (env) => {
     return {
         entry: './src/app.js',
         output: {
-            path:path.join(__dirname,'public','dist '),
-            filename:'/bundle.js' 
+            path:path.join(__dirname,'public','dist'),
+            filename:'bundle.js' 
         },
         module:{
             rules:[{
